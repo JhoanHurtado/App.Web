618fbfc5816cc05b4cbefd75b2909fc6f8a3af02
JhoanHurtado <jhoanezequielh@gmail.com>
2021-06-18 12:11:59 -0500

    Agregar archivos de proyecto.

---
+++ App.Web/Controllers/ClienteController.cs
@@ -0,0 +1,76 @@
+﻿using App.Web.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace App.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private string _URL = "https://localhost:44398/api/";
+        private HttpClient _httpClient = new HttpClient();
+
+        // GET: Cliente
+        public async Task<ActionResult> Index()
+        {
+            var jsonClientes = await _httpClient.GetStringAsync($"{_URL}cliente");
+            var clientes = JsonConvert.DeserializeObject<List<Cliente>>(jsonClientes);
+            ViewBag.Clientes = clientes;
+            return View();
+        }
+
+        public async Task<string> GetClients()
+        {
+            var jsonClientes = await _httpClient.GetStringAsync($"{_URL}cliente");
+            var clientes = JsonConvert.DeserializeObject<List<Cliente>>(jsonClientes);
+            return jsonClientes;
+        }
+
+        // POST: Cliente/Create
+        [HttpPost]
+        public async Task<string> Create([Bind(Include = "Identificador,NombreCompleto")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(cliente), Encoding.UTF8);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                var jsonClientes = await _httpClient.PostAsync($"{_URL}cliente", httpContent);
+                return await GetClients();
+            }
+            return await GetClients();
+        }
+
+        // POST: Cliente/Edit/5
+        [HttpPost]
+        public async Task<string> Edit([Bind(Include = "Identificador,NombreCompleto")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(cliente), Encoding.UTF8);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                var jsonClientes = await _httpClient.PutAsync($"{_URL}cliente", httpContent);
+                return await GetClients();
+            }
+                return await GetClients();
+        }
+
+        // GET: Cliente/Delete/5
+        public async Task<string> Delete(string id)
+        {
+            if (id == null)
+            {
+                return await GetClients();
+            }
+            var jsonClientes = await _httpClient.DeleteAsync($"{_URL}cliente/{id}");
+            return await GetClients();
+
+        }
+
+    }
+}
---
+++ App.Web/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace App.Web.Models
+{
+    public class Cliente
+    {
+        [Key,Required(ErrorMessage = "Identificacion requerid!."), StringLength(16)]
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name ="Identificacion")]
+        public string Identificador { get; set; }
+
+        [Display(Name = "Nombre Completo")]
+        [Required(ErrorMessage = "El nombre es requerido"), StringLength(128)]
+        public string NombreCompleto { get; set; }
+
+    }
+}
